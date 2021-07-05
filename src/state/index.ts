eca6b8b9088d668afa00e81ac5162196a6b7569a
SeihakRithy Muth <Rithy58@gmail.com>
2021-07-04 20:14:03 -0400

    Added npmApi with RTK Query

--- src/state/index.ts
+++ src/state/index.ts
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import { searchSlide } from './slices/searchSlice'
+import { npmApi } from '../services/npmApi'
 
 export const store = configureStore({
-    reducer: searchSlide.reducer
+    reducer: {
+        search: searchSlide.reducer,
+        [npmApi.reducerPath]: npmApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(npmApi.middleware),
 })
 
 export const { search: searchPackage, success: searchPackageSuccess, error: searchPackageError } = searchSlide.actions
